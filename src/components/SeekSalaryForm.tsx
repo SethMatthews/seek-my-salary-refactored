ae8bc18da09a412ae9ea910e25d9bfeb5c484c8d
Seth Matthews <sethmatthews@Seths-MacBook-Pro-2.local>
2023-07-25 16:45:20 +1000

    useState working on input value

--- src/components/SeekSalaryForm.tsx
+++ src/components/SeekSalaryForm.tsx
@@ -1,13 +1,10 @@
 import Image from 'next/image';
 
-// const handleSubmit = (e:Event) => {
-//     e.preventDefault()
-
-//     console.log("Submitted");
-//     console.log(e);
+// interface formProps {
+//     handleSubmit: (submitEvent: React.SyntheticEvent)=>void;
 // }
 
-const SeekSalaryForm = () => {
+const SeekSalaryForm = ({handleSubmit,handleInputChange}:{handleSubmit:(submitEvent: React.SyntheticEvent)=>void, handleInputChange:(submitEvent: React.SyntheticEvent)=>void}) => {
     return (  
         // flex flex-col items-center justify-start
         <div className=' min-h-max w-full bg-sms-purple flex justify-center text-white'>
@@ -17,16 +14,14 @@ const SeekSalaryForm = () => {
                     <Image src={"/seekmysalarylogo.svg"} alt="" height={200} width={500} ></Image>
                 </div>
                 <h2 className='text-center' >Discover the salary range for any listed job on Seek.</h2>
-                <form className='w-full flex flex-col items-stretch gap-4'>
-                    <input className='py-3 rounded-xl text-center'  type="text" id ="add" name="add" required placeholder="Paste the URL of the SEEK job, i.e. https://www.seek.com.au/job/..." />
+                <form onSubmit={handleSubmit} className='w-full flex flex-col items-stretch gap-4'>
+                    <input onChange={handleInputChange} className='py-3 rounded-xl text-center'  type="text" id ="add" name="add" required placeholder="Paste the URL of the SEEK job, i.e. https://www.seek.com.au/job/..." />
                     <button className='bg-sms-aqua py-3 rounded-xl text-sms-purple' >SEEK</button>
                     {/* <input id ="button" name="button" type="submit" value="SEEK"/> */}
                 </form>
                 <h2 className='flex justify-center text-center'>This tool is not connected with Seek Limited. Please note that the listed range entered by the employer may include super..</h2>
             </div>
             
-            
-
         </div>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
 import Head from "next/head";
-import Link from "next/link";
+import { useState } from "react";
+// import Link from "next/link";
 import Footer from "~/components/Footer";
 import SeekSalaryForm from "~/components/SeekSalaryForm";
 
+
+
 export default function Home() {
+
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (submitEvent: React.SyntheticEvent) => {
+    console.log("Submitted");
+    console.log(submitEvent);
+    submitEvent.preventDefault();
+    const e = submitEvent.target as HTMLFormElement;
+    console.log("Submitted");
+    console.log("add are",e.add);
+  }
+  
+  
+  
+  const handleInputChange = (inputChangeEvent: React.SyntheticEvent) => {
+    const inputElement = inputChangeEvent.target as HTMLInputElement;
+    console.log("handleInputChange",inputElement.value);
+    setInputValue(inputElement.value);
+  
+  }
+  
+
+
+
   return (
     <>
       <Head>
@@ -12,10 +40,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <SeekSalaryForm/>
+        <SeekSalaryForm handleSubmit={handleSubmit} handleInputChange={handleInputChange} />
+
+        <p>{inputValue}</p>
+
         <Footer/>
+
     
       </main>
     </>
   );
 }
+
+
